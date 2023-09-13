c07bd91d6cbfc65e6c88ba618937086e3bc0f4b6
charanjeet91 <47344150+charanjeet91@users.noreply.github.com>
2023-09-13 15:00:42 +1000

    Initial commit

---
+++ src/components/Hero.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import logo from "../assets/logo.svg";
+
+const Hero = () => (
+  <div className="text-center hero my-5">
+    <img className="mb-3 app-logo" src={logo} alt="React logo" width="120" />
+    <h1 className="mb-4">Cruise0</h1>
+
+    <p className="lead">
+      Welcome Aboard 
+<p/> 
+Log in to book your travel with Cruise0
+  </p>
+</div>
+);
+
+
+export default Hero;
---
+++ src/utils/contentData.js
@@ -0,0 +1,20 @@
+const contentData = [
+  {
+    title: "New Universal Login vs. Classic Universal Login",
+    link: "https://auth0.com/docs/authenticate/login/auth0-universal-login/new-universal-login-vs-classic-universal-login",
+  },
+  {
+    title: "Socail Connection & User verified email",
+    link: "https://auth0.com/docs/manage-users/user-accounts/verify-emails",
+  },
+  {
+    title: "Cruise0 Branding",
+    link: "https://auth0.com/docs/customize/universal-login-pages",
+  },
+  {
+    title: "Auth0 Dashboard",
+    link: "https://manage.auth0.com/dashboard/us/dev-4aiz8tmhadm3nsy5/applications",
+  }
+];
+
+export default contentData;
